8b65edcdbab0046289ff2f9e8a873982cd21729d
AksaMJML <akaks0186@gmail.com>
2026-02-10 16:51:04 +0530

    created angular project and completed navbar and home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { About } from './page/about/about';
+import { Home } from './page/home/home';
+import { Contact } from './page/contact/contact';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path : "about",
+        component : About
+    },
+    {
+        path : "home",
+        component : Home
+    },
+    {
+        path : "contact",
+        component : Contact
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Nav } from "./page/nav/nav";
+import { Footer } from "./page/footer/footer";
+import { LoginForm } from "./page/login-form/login-form";
+import { FormsModule } from '@angular/forms';
+
+interface Customer{
+  name : string;
+  age : number;
+  address : string;
+  phone : string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Nav, Footer, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('demo-app');
+  customer : Customer = {
+    name : '',
+    age : 0,
+    address : '',
+    phone : ''
+  }
 }
